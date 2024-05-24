2eaca0e1a5f15db732d04dd2a19f909708815bda
Rohan Patil <rohanpatil5122001@gmail.com>
2024-05-24 19:37:02 +0530

    2nd problem added

--- Assignment 3/2.Count frequency.js
+++ Assignment 3/2.Count frequency.js
@@ -1,6 +1,16 @@
 
 let arr = [1,2,2,3,3,3,4,4,4,4]
 
- const result = new Set(arr)
+ const result = {}
 
- console.log(result);
+for (let i = 0; i < arr.length; i++) {
+    
+    if (!result[arr[i]]) {
+         result[arr[i]] = 1
+    }else{
+        result[arr[i]] =  result[arr[i]] + 1
+    }
+    
+}
+
+console.log(result);
